9a3bffa88cacdec806cf6bcac54638c9a3529936
fedykvitalik2004 <90273574+fedykvitalik2004@users.noreply.github.com>
2024-12-13 16:16:58 +0200

    Add openapi specification

---
+++ src/test/java/org/vitalii/fedyk/librarygenerated/LibraryGeneratedApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vitalii.fedyk.librarygenerated;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryGeneratedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
